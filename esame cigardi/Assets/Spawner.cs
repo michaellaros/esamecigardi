fa30bff352c3b619158b84e39ba5154004d44ad5
michaellaros <michael_larosa@studenti.naba.it>
2021-02-06 19:33:32 +0100

    animation work correctly

--- esame cigardi/Assets/Spawner.cs
+++ esame cigardi/Assets/Spawner.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public GameObject objectToSpawn;
+    public Vector3 positiontospawn = new Vector3(31.9f, 8.4f, 54.84f);
+    public float tempodispawn = 0f;
+    public float startingTime = 5f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        tempodispawn = startingTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        tempodispawn -= 1 * Time.deltaTime;
+
+        if (tempodispawn<=0)
+        {
+            Instantiate(objectToSpawn, positiontospawn, Quaternion.identity);
+            tempodispawn = 5f;
+        }
     }
 }
