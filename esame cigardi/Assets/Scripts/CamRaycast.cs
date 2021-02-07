a1987d7bd1874aaa4d002de78d5fd2b40b0db438
DavideWu <56259661+DavideWu@users.noreply.github.com>
2021-02-07 21:14:20 +0100

    script CamRaycast

---
+++ esame cigardi/Assets/Scripts/CamRaycast.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRaycast : MonoBehaviour
+{
+    public GameObject HitP;
+
+    void Start()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit" + hit.collider.gameObject.name);
+            HitP.transform.position = hit.point;
+            HitP.SetActive(true);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
+}
