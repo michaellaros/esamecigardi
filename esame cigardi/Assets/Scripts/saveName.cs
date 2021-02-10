9d6cbc2c722e093b2acdf0c9c564d2b13e8d8339
DavideWu <56259661+DavideWu@users.noreply.github.com>
2021-02-10 15:04:00 +0100

    script SaveName

---
+++ esame cigardi/Assets/Scripts/saveName.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class saveName : MonoBehaviour
+{
+    public GameObject inputField;
+    public Text OutputTx;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OutputTx.text = "Please insert your name";
+        if (PlayerPrefs.GetInt("Named") == 1)
+        {
+            inputField.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SavePlayerName()
+    {
+        if (inputField.GetComponent<Text>().text.ToString() != null)
+        {
+            PlayerPrefs.SetString("PlayerName", inputField.GetComponent<Text>().text);
+            PlayerPrefs.SetInt("Named", 1);
+            OutputTx.text = "Name saved";
+        }
+        else
+        {
+            OutputTx.text = "Please insert your name";
+        }
+        
+    }
+}
