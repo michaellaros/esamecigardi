1748c3d1b119c3f34c724c011b407be36c4cd8cf
michaellaros <michael_larosa@studenti.naba.it>
2021-02-16 20:09:05 +0100

    camera work

---
+++ esame cigardi/Assets/Scripts/RaycastTest.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+
+public class RaycastTest : MonoBehaviour
+{
+    ARRaycastManager MyRaycastManager;
+    List<ARRaycastHit> planesHitList = new List<ARRaycastHit>();
+    public GameObject prefabtoinstantiate;
+    GameObject instantiatedObject;
+    bool deactivatePlanesOnce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && instantiatedObject == null)
+        {
+            Vector2 touchPosition = Input.GetTouch(0).position;
+            if (MyRaycastManager.Raycast(touchPosition, planesHitList, TrackableType.PlaneWithinPolygon))
+            {
+                Pose planeHitPose = planesHitList[0].pose;
+                instantiatedObject = Instantiate(prefabtoinstantiate, planeHitPose.position, planeHitPose.rotation);
+            }
+        }
+        if (instantiatedObject != null)
+        {
+            if (!deactivatePlanesOnce)
+            {
+                Planesmanager(true);
+                deactivatePlanesOnce = true;
+            } 
+        }
+        else
+        {
+            if(deactivatePlanesOnce)
+        {
+                deactivatePlanesOnce = false;
+        }
+                
+                    }
+            
+
+    }
+    void Planesmanager(bool status)
+    {
+        GameObject[] allPlanesInScene = GameObject.FindGameObjectsWithTag("ARPlanes");
+        for (int i = 0; i < allPlanesInScene.Length; i++)
+        {
+            allPlanesInScene[i].SetActive(false);
+        }
+    }
+}
